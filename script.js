5488d124d0a63d47ec32aadff34747ab548a8ea5
BertoAguilar <150314027+BertoAguilar@users.noreply.github.com>
2024-02-12 16:57:50 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var likeCount1 = 9;
+var likesElement1 = document.querySelector("#likes1");
+
+function addLike1() {
+  likeCount1++;
+  likesElement1.innerText = likeCount1 + " like(s)!";
+}
+
+var likeCount2 = 12;
+var likesElement2 = document.querySelector("#likes2");
+
+function addLike2() {
+  likeCount2++;
+  likesElement2.innerText = likeCount2 + " like(s)!";
+}
+
+var likeCount3 = 9;
+var likesElement3 = document.querySelector("#likes3");
+
+function addLike3() {
+  likeCount3++;
+  likesElement3.innerText = likeCount3 + " like(s)!";
+}
